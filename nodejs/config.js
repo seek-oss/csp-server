574e669f7f75cc52be336fe66c362c36256b4ba0
Pierrick Brossin <github@bs-network.net>
2017-09-12 13:48:09 +0200

    add config option to change elasticsearch index

--- nodejs/config.js
+++ nodejs/config.js
@@ -6,7 +6,8 @@ module.exports = {
   AppName: 'csp-server',
   LogFileName: process.env.LogFileName || "./csp-server.json",
   LogLevel: process.env.LogLevel || "warn",
-  ElasticSearchHost: process.env.ElasticSearchHost || "https://search-csp-server-********.****region****.es.amazonaws.com",
   Port: process.env.PORT || 9000,
-  ElasticSearchVersion: process.env.ElasticSearchVersion || "master"
+  ElasticSearchHost: process.env.ElasticSearchHost || "https://search-csp-server-********.****region****.es.amazonaws.com",
+  ElasticSearchVersion: process.env.ElasticSearchVersion || "master",
+  ElasticSearchIndex: process.env.ElasticSearchIndex || "cspdata"
 };
